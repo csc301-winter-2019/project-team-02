ecc0d9a1deb3cdaf272bbd6d87e76efa763e52d6
Kumar Damani <kumar.damani@mail.utoronto.ca>
2019-02-26 22:51:08 -0700

    fixed default zoom level for map

--- web/public/javascripts/map.js
+++ web/public/javascripts/map.js
@@ -8,7 +8,7 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 13,
+	maxZoom: 18,
 	ext: 'png'
 }).addTo(map);
 
@@ -17,3 +17,5 @@ var myDataPoint = L.marker([43.6529, -79.3849]).addTo(map);
 
 // Bind popup to Data Point object
 myDataPoint.bindPopup("<h3>City Hall (not really)</h3><p>Toronto, ON<br>Information about city hall.</p>");
+
+map.setZoom(13);
