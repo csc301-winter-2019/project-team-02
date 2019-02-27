fdb6e5c171da5ebaf91d09c84647d95ce4e530ca
Kumar Damani <kumar.damani@mail.utoronto.ca>
2019-02-26 22:27:26 -0700

    fixed spaces to indents

--- web/public/javascripts/map.js
+++ web/public/javascripts/map.js
@@ -5,17 +5,15 @@
 
 // different basemap
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-			subdomains: 'abcd',
-			minZoom: 0,
-			maxZoom: 13,
-			ext: 'png'
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 13,
+	ext: 'png'
 }).addTo(map);
 
 // Create point feature for somewhere downtown
 var myDataPoint = L.marker([43.6529, -79.3849]).addTo(map);
 
 // Bind popup to Data Point object
-// myDataPoint.bindPopup("This is my city");
-// i can add regular html to this too - neat!
 myDataPoint.bindPopup("<h3>City Hall (not really)</h3><p>Toronto, ON<br>Information about city hall.</p>");
